d528e43b2331f01583b64e0be79c0fc756ce5ccc
Coimbra <adsjkbd@gmail.com>
2024-01-17 08:47:58 -0300

    Diretiva NgIf

---
+++ directives/diretivas/Anotações.md
@@ -0,0 +1,10 @@
+# Diretivas
+
+## NGIF
+
+- A diretiva ngIf é usada para condicionalmente renderizar ou destruir elementos HTML com base em uma expressão booleana. Se a expressão for avaliada como verdadeira, o elemento associado será renderizado; caso contrário, o elemento será removido do DOM.
+
+## NGIF X HIDDEN
+
+- [HIDDEN] : recomendado para árvore de elementos pequenos
+- [HIDDEN] : recomendado para árvore de elementos grandes
---
+++ directives/diretivas/src/app/diretiva-ngif/diretiva-ngif.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-diretiva-ngif',
+  templateUrl: './diretiva-ngif.component.html',
+  styleUrls: ['./diretiva-ngif.component.css']
+})
+export class DiretivaNgifComponent {
+
+  cursos:String[] = ['Angular', 'Java', 'JS', 'Python'];
+  mostrarCursos: boolean = false;
+
+  ngOnInit(){}
+
+  onMostrarCursos () {
+    this.mostrarCursos = !this.mostrarCursos;
+  }
+}

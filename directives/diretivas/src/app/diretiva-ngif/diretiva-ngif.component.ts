0d0a158387dca74ff4752ae3c2adf22c37a27d0b
Coimbra <adsjkbd@gmail.com>
2024-01-23 06:15:14 -0300

    Diretivas: sobre o * e template

--- directives/diretivas/src/app/diretiva-ngif/diretiva-ngif.component.ts
+++ directives/diretivas/src/app/diretiva-ngif/diretiva-ngif.component.ts
@@ -3,16 +3,15 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-diretiva-ngif',
   templateUrl: './diretiva-ngif.component.html',
-  styleUrls: ['./diretiva-ngif.component.css']
+  styleUrls: ['./diretiva-ngif.component.css'],
 })
 export class DiretivaNgifComponent {
-
-  cursos:String[] = ['Angular', 'Java', 'JS', 'Python'];
+  cursos: String[] = ['Angular', 'Java', 'JS', 'Python'];
   mostrarCursos: boolean = false;
 
-  ngOnInit(){}
+  ngOnInit() {}
 
-  onMostrarCursos () {
+  onMostrarCursos() {
     this.mostrarCursos = !this.mostrarCursos;
   }
 }

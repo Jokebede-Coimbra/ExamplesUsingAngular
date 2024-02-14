d151927be6584ddbb24fb9b9eec77fb188ce5cc9
Coimbra <adsjkbd@gmail.com>
2024-02-14 07:07:38 -0300

    Emitindo Eventos com Output properties

--- data-binding/src/app/data-binding/data-binding.component.ts
+++ data-binding/src/app/data-binding/data-binding.component.ts
@@ -6,17 +6,23 @@ import { Component } from '@angular/core';
   styleUrls: ['./data-binding.component.css'],
 })
 export class DataBindingComponent {
-  urlImagem: string = "assets/img/borboleta.jpg";
+  urlImagem: string = 'assets/img/borboleta.jpg';
   nome: string = 'Coimbra';
   cursoAngular: boolean = true;
 
-  nomeDoCurso: string = "Angular";
-  
+  nomeDoCurso: string = 'Angular';
+
+  valorInicial = 10;
+
   getValor() {
     return 1;
   }
 
   getCurtirCurso() {
     return true;
+  }
+
+  onMudouValor(evento: any) {
+    console.log(evento.novoValor);
   }
 }

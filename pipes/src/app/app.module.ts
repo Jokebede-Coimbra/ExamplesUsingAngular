7555b9e8aaab8e3d06e6f25b7dbb3cc11afc06ca
Coimbra <adsjkbd@gmail.com>
2024-02-20 05:53:44 -0300

    Pipes: Async (assíncrono

--- pipes/src/app/app.module.ts
+++ pipes/src/app/app.module.ts
@@ -7,11 +7,19 @@ import { ExemplosPipesComponent } from './exemplos-pipes/exemplos-pipes.componen
 import { CamelCasePipe } from './camel-case.pipe';
 import { registerLocaleData } from '@angular/common';
 import ptBr from '@angular/common/locales/pt';
-import { SettingsService } from './settings.service';
+import { FormsModule } from '@angular/forms';
+import { FiltroArrayPipe } from './filtro-array.pipe';
+import { FiltroArrayImpuroPipe } from './filtro-array-impuro.pipe';
 registerLocaleData(ptBr);
 @NgModule({
-  declarations: [AppComponent, ExemplosPipesComponent, CamelCasePipe],
-  imports: [BrowserModule, AppRoutingModule],
+  declarations: [
+    AppComponent,
+    ExemplosPipesComponent,
+    CamelCasePipe,
+    FiltroArrayPipe,
+    FiltroArrayImpuroPipe,
+  ],
+  imports: [BrowserModule, AppRoutingModule, FormsModule],
   providers: [
     /*{
        provide: LOCALE_ID,
@@ -19,13 +27,13 @@ registerLocaleData(ptBr);
 
 },
 */
-   /* SettingsService,
+    /* SettingsService,
     {
       provide: LOCALE_ID,
       deps: [SettingsService],
       useFactory: ptbrLocale
     }*/
-    { provide: LOCALE_ID, useValue: 'pt' }
+    { provide: LOCALE_ID, useValue: 'pt' },
   ],
   bootstrap: [AppComponent],
 })
--- pipes/src/app/exemplos-pipes/exemplos-pipes.component.ts
+++ pipes/src/app/exemplos-pipes/exemplos-pipes.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Observable, interval } from 'rxjs';
+import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-exemplos-pipes',
@@ -6,14 +8,48 @@ import { Component } from '@angular/core';
   styleUrls: ['./exemplos-pipes.component.css'],
 })
 export class ExemplosPipesComponent {
-
   livro: any = {
     titulo: 'Refatoração',
     rating: 4.9,
     numeroPaginas: 456,
     preco: 82.49,
     dataLancamento: new Date(2020, 4, 30),
-    url: 'https://www.amazon.com.br'
+    url: 'https://www.amazon.com.br',
   };
 
+  filtro!: string;
+
+  livros: string[] = ['Java', 'Angular2'];
+
+  valorAsync = new Promise((resolve, reject) => {
+    setTimeout(() => resolve('Valor assíncrono'), 2000);
+  });
+
+  valorAsync2 = new Observable<string>(observable => {
+    setTimeout(() => {
+      observable.next('Valor assíncrono 2');
+    }, 2000);
+  });
+
+  addCurso(valor: string) {
+    this.livros.push(valor);
+    console.log(this.livros);
+  }
+
+
+  obterCursos() {
+    if (
+      this.livros.length === 0 ||
+      this.filtro === undefined ||
+      this.filtro.trim() === ''
+    ) {
+      return this.livros;
+    }
+
+    return this.livros.filter((v) =>
+      v.toLocaleLowerCase().includes(this.filtro.toLocaleLowerCase())
+    );
+  }
+  ngOnInit() {
+  }
 }

04996fad8692ddc509fbf993b81c944b1d21ba22
Coimbra <adsjkbd@gmail.com>
2024-03-11 14:11:46 -0300

    Rotas Filhas: desenvolvendo as telas

--- rotas/src/app/alunos/aluno-detalhe/aluno-detalhe.component.ts
+++ rotas/src/app/alunos/aluno-detalhe/aluno-detalhe.component.ts
@@ -1,10 +1,35 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Subscription } from 'rxjs';
+import { AlunosService } from '../alunos.service';
 
 @Component({
   selector: 'app-aluno-detalhe',
   templateUrl: './aluno-detalhe.component.html',
-  styleUrls: ['./aluno-detalhe.component.css']
+  styleUrls: ['./aluno-detalhe.component.css'],
 })
 export class AlunoDetalheComponent {
+  aluno: any;
+  inscricao!: Subscription;
 
+  constructor(
+    private route: ActivatedRoute,
+    private router: Router,
+    private alunosService: AlunosService
+  ) {}
+
+  ngOnInit() {
+    this.inscricao = this.route.params.subscribe((params) => {
+      let id = params['id'];
+      this.aluno = this.alunosService.getAluno(id);
+    });
+  }
+
+  ngOnDestroy(): void {
+    this.inscricao.unsubscribe();
+  }
+
+  editarContato() {
+    this.router.navigate(['/alunos', this.aluno.id, 'editar']);
+  }
 }
--- rotas/src/app/alunos/aluno-form/aluno-form.component.ts
+++ rotas/src/app/alunos/aluno-form/aluno-form.component.ts
@@ -1,10 +1,34 @@
 import { Component } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { AlunosService } from '../alunos.service';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-aluno-form',
   templateUrl: './aluno-form.component.html',
-  styleUrls: ['./aluno-form.component.css']
+  styleUrls: ['./aluno-form.component.css'],
 })
 export class AlunoFormComponent {
+  aluno: any = {};
+  inscricao!: Subscription;
 
+  constructor(
+    private route: ActivatedRoute,
+    private alunosService: AlunosService
+  ) {}
+
+  ngOnInit() {
+    this.inscricao = this.route.params.subscribe((params) => {
+      let id = params['id'];
+      this.aluno = this.alunosService.getAluno(id);
+
+      if (this.aluno == null) {
+        this.aluno = {};
+      }
+    });
+  }
+
+  ngOnDestroy() {
+    this.inscricao.unsubscribe();
+  }
 }

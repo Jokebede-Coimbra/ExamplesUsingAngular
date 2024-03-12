59948491dbaefdc0d809004eccee9d494d5fc5ac
Coimbra <adsjkbd@gmail.com>
2024-03-12 07:27:51 -0300

    Carregamento sob demanda (lazy loading)

--- rotas/src/app/app.module.ts
+++ rotas/src/app/app.module.ts
@@ -7,18 +7,10 @@ import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
 
 import { FormsModule } from '@angular/forms';
-import { CursosModule } from './cursos/cursos.module';
-import { AlunosModule } from './alunos/alunos.module';
 
 @NgModule({
   declarations: [AppComponent, HomeComponent, LoginComponent],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    FormsModule,
-    CursosModule,
-    AlunosModule,
-  ],
+  imports: [BrowserModule, AppRoutingModule, FormsModule],
 
   bootstrap: [AppComponent],
 })

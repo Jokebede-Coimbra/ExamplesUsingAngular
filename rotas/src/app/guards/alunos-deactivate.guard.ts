c771805398f5e412ddd7e8c9cc1158a231d42102
Coimbra <adsjkbd@gmail.com>
2024-03-20 19:01:53 -0300

    Usando Guarda de Rotas: CanDeactivate

--- rotas/src/app/guards/alunos-deactivate.guard.ts
+++ rotas/src/app/guards/alunos-deactivate.guard.ts
@@ -1,7 +1,11 @@
 import { AlunoFormComponent } from '../alunos/aluno-form/aluno-form.component';
 import { Observable } from 'rxjs';
 import { Injectable } from '@angular/core';
-import { CanDeactivate, ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
+import {
+  CanDeactivate,
+  ActivatedRouteSnapshot,
+  RouterStateSnapshot,
+} from '@angular/router';
 
 @Injectable()
 export class AlunosDeactivateGuard
@@ -12,10 +16,7 @@ export class AlunosDeactivateGuard
     route: ActivatedRouteSnapshot,
     state: RouterStateSnapshot
   ): Observable<boolean> | Promise<boolean> | boolean {
-    // Aqui você pode implementar a lógica para verificar se pode ou não desativar a rota
-    // Por exemplo, pode verificar se o formulário do aluno está sujo (alterado) e perguntar ao usuário se deseja sair sem salvar
-    // Se a rota puder ser desativada, retorne true, caso contrário, retorne false ou uma Promise/Observable que resolva para false
     console.log('guarda de desativação');
-    return component.podeDesativar? component.podeDesativar(): true;
+    return component.podeDesativar ? component.podeDesativar() : true;
   }
 }

fb42598a0a7575e7a18067c040b4a2aff991e6a5
Coimbra <adsjkbd@gmail.com>
2024-03-30 15:52:09 -0300

    Verificando dados do Form no template com JSON

--- formulario/src/app/template-form/template-form.component.ts
+++ formulario/src/app/template-form/template-form.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-template-form',
@@ -8,12 +8,11 @@ import { Component, OnInit } from '@angular/core';
 export class TemplateFormComponent implements OnInit {
   usuario: any = {
     nome: null,
-    email: null
+    email: null,
   };
   onSubmit(form: any) {
     console.log(form);
     console.log(this.usuario);
-
   }
 
   ngOnInit() {}

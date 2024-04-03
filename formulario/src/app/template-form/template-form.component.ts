1bdc2205eed78626fedb2b7141eca8c17a9a74c4
Coimbra <adsjkbd@gmail.com>
2024-04-03 07:35:40 -0300

    Forms (template driven) Refatorando (simplificando) CSS e mensagens de erro

--- formulario/src/app/template-form/template-form.component.ts
+++ formulario/src/app/template-form/template-form.component.ts
@@ -16,4 +16,14 @@ export class TemplateFormComponent implements OnInit {
   }
 
   ngOnInit() {}
+
+  verificaValidTouched(campo: any) {
+    return !campo.valid && campo.touched;
+  }
+  aplicaCssErro(campo: any) {
+    return {
+      'has-error': this.verificaValidTouched(campo),
+      'has-feedback': this.verificaValidTouched(campo),
+    };
+  }
 }

6fb89661727780d88f3dfa3b9e0d97b8ac0abc93
Coimbra <adsjkbd@gmail.com>
2024-04-16 06:57:11 -0300

    Formulários reativos: Fazendo submit

--- formulario/src/app/data-form/data-form.component.ts
+++ formulario/src/app/data-form/data-form.component.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormControl, FormGroup } from '@angular/forms';
 
@@ -7,19 +8,24 @@ import { FormBuilder, FormControl, FormGroup } from '@angular/forms';
   styleUrls: ['./data-form.component.css'],
 })
 export class DataFormComponent implements OnInit {
-
   formulario!: FormGroup;
 
-  constructor(private formBuilder: FormBuilder) {}
+  constructor(private formBuilder: FormBuilder, private http: HttpClient) {}
 
   ngOnInit() {
-    /*this.formulario = new FormGroup({
-      nome:  new FormControl(null),
-      email:  new FormControl(null)
-    });*/
-    this.formulario  = this.formBuilder.group({
+    this.formulario = this.formBuilder.group({
       nome: [null],
-      email: [null]
+      email: [null],
     });
+  }
+
+  onSubmit() {
+    console.log(this.formulario.value);
+
+    this.http
+      .post('https://httpbin.org/post', JSON.stringify(this.formulario.value))
+      .subscribe((dados: any) => {
+        console.log(dados);
+      });
   }
 }

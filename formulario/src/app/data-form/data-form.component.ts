5f5980e418cb6eb5064b8beeb17a1f1940ce0f20
Coimbra <adsjkbd@gmail.com>
2024-04-18 07:03:32 -0300

    Formulários reativos: Endereço (migrando de template driven para form reativo)

--- formulario/src/app/data-form/data-form.component.ts
+++ formulario/src/app/data-form/data-form.component.ts
@@ -21,6 +21,13 @@ export class DataFormComponent implements OnInit {
     this.formulario = this.formBuilder.group({
       nome: [null, Validators.required],
       email: [null, [Validators.required, Validators.email]],
+      cep:[null, Validators.required],
+      numero:[null, Validators.required],
+      complemento:[null],
+      rua:[null, Validators.required],
+      bairro:[null, Validators.required],
+      cidade:[null, Validators.required],
+      estado:[null, Validators.required]
     });
   }
 
@@ -40,13 +47,13 @@ export class DataFormComponent implements OnInit {
     this.formulario.reset();
   }
 
-  verificaValidTouched(campo: any) {
+  verificaValidTouched(campo: string) {
     return (
-      !this.formulario.get(campo)?.valid && this.formulario.get(campo)?.touched
+      !this.formulario.get(campo)?.valid && !!this.formulario.get(campo)?.touched
     );
   }
 
-  aplicaCssErro(campo: any) {
+  aplicaCssErro(campo: string) {
     return {
       'has-error': this.verificaValidTouched(campo),
       'has-feedback': this.verificaValidTouched(campo),
